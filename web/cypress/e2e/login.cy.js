09f13fd93aea4901b5e83b39ff7ba0b1636fdcb1
Willian JS <willianjunios32@gmail.com>
2026-03-12 07:30:11 -0300

    Treinamento de hoje

--- web/cypress/e2e/login.cy.js
+++ web/cypress/e2e/login.cy.js
@@ -1,15 +1,38 @@
 describe('Login', ()=>{
-  it('Deve logar com sucesso', ()=>{
+
+  function getFormattedTodayDate() {
+    const today = new Date();
+
+    const day = String(today.getDate()).padStart(2, '0');
+    const month = String(today.getMonth() + 1).padStart(2, '0'); 
+    const year = today.getFullYear();
+
+    return `${day}/${month}/${year}`;
+  }
+
+  it.only('Deve logar com sucesso', ()=>{
     cy.start()
     cy.submitLoginForm('papito@webdojo.com', 'katana123')
 
     cy.get('[data-cy="user-name"]')
+
       .should('be.visible')
       .and('have.text', 'Fernando Papito')
 
     cy.get('[data-cy="welcome-message"]')
       .should('be.visible')
       .and('have.text', 'Olá QA, esse é o seu Dojo para aprender Automação de Testes.')
+
+    cy.getCookie('login_date').should('exist')
+    
+    cy.getCookie('login_date').should((cookie)=> {
+      expect(cookie.value).to.eq(getFormattedTodayDate())
+    })
+
+    cy.window().then((win) => {
+        const token = win.localStorage.getItem('token')
+        expect(token).to.match(/^[a-fA-F0-9]{32}$/)
+    })
   })
 
   it('Não deve logar com senha inválida', ()=>{
